10eb7c2520d1bd579f4bf59eba3fe217a2671725
SankariMano <sankarimano97@gmail.com>
2017-07-05 10:38:28 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Print {
+
+	public static void main(String[] args)throws IOException {
+		
+		for(int i=0;i<5;i++){
+			System.out.println("Hello");
+		}
+
+	}
+
+}
