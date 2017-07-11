adb259143371b0ee66dbaa1d0e334a76f52a102a
SankariMano <sankarimano97@gmail.com>
2017-07-11 23:14:48 +0530

    Update and rename Reverseint.java to Convertint.java

--- Convertint.java
+++ Convertint.java
@@ -1,7 +1,7 @@
 
 
 import java.io.*;
-public class Reverseint{
+public class Convertint{
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the string");
