b09dc0406da50fc71cca42f806db8ad1a38e385e
SankariMano <sankarimano97@gmail.com>
2017-07-11 23:31:19 +0530

    Update and rename Reverseint.java to Revsinteger.java

--- Revsinteger.java
+++ Revsinteger.java
@@ -1,6 +1,6 @@
 
 import java.io*;
-public class Reverseint {
+public class Revsinteger {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the number");
